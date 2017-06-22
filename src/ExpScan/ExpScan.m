4f07bb6101602d5277dfce93141e4a4f80a14f9a
Antonio Pifferi <antonio.pifferi@polimi.it>
2017-06-22 09:14:51 +0200

    Added ExpScan

---
+++ src/ExpScan/ExpScan.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ExpScan                                                               %
+%                                                                       %
+% v1:                                                                   %
+% Create Exp Matrix to be loaded on DOT based on PTB scan data
+% See AntonioNotes under Data\ScanHead 
+% Based on previous lost ScanHead
+%                                                                       %
+% A. Pifferi 22/06/2017                                                  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
---
+++ src/ExpScan/ExpScan_ini.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Ini File for ExpScan                                                  %
+%                                                                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% FILES
+PathServer='D:\Beta\';
+PathDataIn='ScanHead\for_polimi_dat\';
+PathDataOut='Simulations\Solus\data\201612\';
+FileNameIn='S04_2_DTOFarray_6D_single_binxy_nobs_blocksum.mat';
+FileNameOut='EXP_DATA_EXP.mat';
+
