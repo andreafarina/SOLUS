9b5800e579ef11b19ad1c33b3cf91682acb8b0db
Edoardo Ferocino <edoardo.ferocino@polimi.it>
2017-10-17 22:16:23 +0200

    Doesn't show warnings if using only DOT_core to make simulations

--- src/multiSimProcedures/Quantification_MultiSim.m
+++ src/multiSimProcedures/Quantification_MultiSim.m
@@ -1,12 +1,14 @@
-warning('off','backtrace');
-if (~EXP_DATA) == 1
-    if exist('errXP','var'), F(errXP).Value(iL) = Q.COM.error(1); end
-    if exist('errYP','var'), F(errYP).Value(iL) = Q.COM.error(2);end
-    if exist('errZP.Value','var'), F(errZP).Value(iL) = Q.COM.error(3); end
-    if exist('relMUAP','var'), F(relMUAP).Value(iL) = Q.max.error; end
-    if exist('relVOL','var'), F(relVOL).Value(iL) = Q.volume.rel_error; end
-else
-    warning('No quantification without reference');
-    for iF=1:numel(F), F(iF).Value(iL) = 0; end %#ok<SAGROW>
+if exist('isComingFromInterface','var')
+    warning('off','backtrace');
+    if (~EXP_DATA) == 1
+        if exist('errXP','var'), F(errXP).Value(iL) = Q.COM.error(1); else, warning('errXP not calculated'); end
+        if exist('errYP','var'), F(errYP).Value(iL) = Q.COM.error(2);else, warning('errYP not calculated'); end
+        if exist('errZP','var'), F(errZP).Value(iL) = Q.COM.error(3);else, warning('errZP not calculated'); end
+        if exist('relMUAP','var'), F(relMUAP).Value(iL) = Q.max.rel_error;else, warning('relMUAP not calculated'); end
+        if exist('relVOL','var'), F(relVOL).Value(iL) = Q.volume.rel_error;else, warning('relVOL not calculated'); end
+    else
+        warning('No quantification without reference');
+        for iF=1:numel(F), F(iF).Value(iL) = 0; end %#ok<SAGROW>
+    end
+    warning('on','backtrace');
 end
-warning('on','backtrace');
