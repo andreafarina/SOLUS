6a46ac934a21e5887c8d92bb70d66f9f218e2709
Andrea Farina <andrea.farina@polimi.it>
2017-10-25 10:45:35 +0200

    Fixed a bug fo Gfun.m that gave errors with Matlab version earlier than 2016b

--- src/subroutines/Gfun.m
+++ src/subroutines/Gfun.m
@@ -3,9 +3,14 @@ mu = [p(3),p(4),p(5)];
     cov_matrix = [p(6),p(7),p(8);
     p(7),p(9),p(10);
     p(8),p(10),p(11)];
-G = @(A,b,mu,cov_matrix)(b+(exp(-1/2*sum((space-mu)/(cov_matrix).*(space-mu),2))*...
+%% with automatic implicit expansion, compatible with R0216a and later
+% G = @(A,b,mu,cov_matrix)(b+(exp(-1/2*sum((space-mu)/(cov_matrix).*(space-mu),2))*...
+%     1./(sqrt((2*pi)^3*det(cov_matrix)))*A));
+%% compatible with earlier
+space_off = space - repmat(mu,[size(space,1),1]);
+G = @(A,b,mu,cov_matrix)(b+(exp(-1/2*sum((space_off)/(cov_matrix).*(space_off),2))*...
     1./(sqrt((2*pi)^3*det(cov_matrix)))*A));
-
+%%
 y = G(p(1),p(2),mu,cov_matrix);
 y = reshape(y,size_space);
 end
