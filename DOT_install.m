e7b40a792a898e6b86feb831d4a05d6b0401444f
Andrea Farina <andrea.farina@polimi.it>
2017-11-06 12:24:29 +0100

    added DOT_install.m that set environment variable and paths. It is mandatory to run this file before a DOT session.

---
+++ DOT_install.m
@@ -0,0 +1,31 @@
+% Add path and set environment variables.
+
+disp('Setting environment variable DOTDIR');
+setenv('DOTDIR',pwd);
+disp('Setting environment variable DOTSRC');
+
+setenv('DOTSRC',[getenv('DOTDIR'),filesep,'src']);
+disp('Adding path');
+addpath(getenv('DOTSRC'));
+addpath([getenv('DOTSRC'),filesep,'util']);
+d = dir([getenv('DOTSRC'),filesep,'util']);
+names = {d.name};
+isdir = {d.isdir};
+num_names = size(names,2);
+n_name = size(names,2);
+for i=3:n_name
+    f = names{i};
+    if isdir{i}
+        addpath([getenv('DOTSRC'),filesep,'util',...
+            filesep,f]);
+    end
+end
+addpath([getenv('DOTSRC'),filesep,'fwd']);
+addpath([getenv('DOTSRC'),filesep,'subroutines']);
+addpath([getenv('DOTSRC'),filesep,'solvers']);
+addpath([getenv('DOTSRC'),filesep,'experimental']);
+addpath([getenv('DOTSRC'),filesep,'visual']);
+addpath([getenv('DOTSRC'),filesep,'multiSimProcedures']);
+clearvars;
+
+
