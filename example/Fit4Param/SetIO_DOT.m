56fb0eaa3c461af718a818ded457ec01d8edb80b
gdisciac <gdisciac@nadder.cs.ucl.ac.uk>
2018-05-02 10:20:35 +0100

    Added folder example/Fit4Param displaying an example of use of reconstruction using toast++ from another toast++ simulation (stored in  TOAST_sim/TEST_Fit4Param.mat)

---
+++ example/Fit4Param/SetIO_DOT.m
@@ -0,0 +1,6 @@
+filename = 'TEST_Fit4Param';%myname 
+session = '201710';
+exp_path = './';%['../data/',session,'/'];
+res_path = './';%['../results/',session,'/'];
+exp_file = 'GatedSiPM';
+%exp_file = 'SOLUS_TEST';
