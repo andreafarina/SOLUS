26038f8121a286041de328fc8b30efffa842d526
Giuseppe Di Sciacca <giuseppe.sciacca.17@ucl.ac.uk>
2020-04-05 14:56:49 +0100

    solved merged conflicts

--- src/fwd/ForwardTD_multi_wave.m
+++ src/fwd/ForwardTD_multi_wave.m
@@ -1,6 +1,5 @@
 function [phi,Area]=ForwardTD_multi_wave(grid,SourcePos,DetectorPos, dmask,...
         muaB,muspB,nB,Mua,Musp,A, dt, nstep, self_norm, geom, TYPE_FWD, radiometry)
-<<<<<<< HEAD
 nQM = sum(dmask(:));   
 phi = zeros(nstep,nQM*radiometry.nL);
 Area = zeros(nQM,radiometry.nL);
@@ -30,32 +29,3 @@ Area = zeros(nQM,radiometry.nL);
     end
 
 end
-=======
-
-nQM = sum(dmask(:));   
-phi = zeros(nstep,nQM*radiometry.nL);
-Area = zeros(nQM,radiometry.nL);
-for inl = 1:radiometry.nL
-    fprintf(['<strong>------- Wavelength ',num2str(radiometry.lambda(inl)),'-------</strong>\n'])
-    meas_set = (1:nQM)+(inl-1)*nQM;
-    if isempty(Mua)||isempty(Musp)
-        MuaData = []; MusData = [];
-    else
-        MuaData = squeeze(Mua(:,:,:,inl));
-        MusData = squeeze(Musp(:,:,:,inl));
-    end
-    [phi(:,meas_set),Area(1:nQM,inl)] = ForwardTD(grid,SourcePos, DetectorPos, dmask,...
-        muaB(inl), muspB(inl),nB, MuaData,...
-        MusData, A, dt,...
-        nstep, self_norm, geom, TYPE_FWD);
-    dummy_phi = phi(:,meas_set);
-    if sum(dummy_phi(:)<0) > 0
-        warning('off','verbose')
-        warning('off','backtrace')
-        warning([num2str(sum(dummy_phi(:)<0)),' elements of DataTD < 0!']);
-        warning('on','verbose')
-        warning('on','backtrace')
-    end
-end
-end
->>>>>>> 601f76d0bf4879d27e7741946f367a2a80e5bb98
--- src/subroutines/SelectROI.m
+++ src/subroutines/SelectROI.m
@@ -2,21 +2,13 @@ function roi = SelectROI(data,irf)
 % plot time-domain data y, allow user to select a ROI. 
 % If the irf is provided it will be plotted as weel (in green)
 
-<<<<<<< HEAD
-figure();
-=======
 figure;
->>>>>>> 601f76d0bf4879d27e7741946f367a2a80e5bb98
 semilogy(data),hold on
 if nargin > 1
     semilogy(irf/max(irf)*max(data(:)),'g')
 end
 ylim([max(data(:))/10000 max(data(:))])%,legend(['data','irf']),
-<<<<<<< HEAD
 title('select ROI');
-=======
-title('select ROI')
->>>>>>> 601f76d0bf4879d27e7741946f367a2a80e5bb98
 [x,~] = ginput(2);
 roi = round(x);
 disp(['Used ROI: ',num2str(roi')]);
